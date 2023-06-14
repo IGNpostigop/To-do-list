109565e321d9d19d41bd70bda499b8d9b5388687
Ignacio Postigo Pérez <ignpostigop@gmail.com>
2023-06-14 22:20:46 +0200

    feat: añadir tarea

--- main.js
+++ main.js
@@ -1,24 +1,67 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+//Creo una variable donde guardo el boton que lo busco con el querySelector pasandole # para id y . para class
+const btnAdd = document.querySelector('#agregar-tarea')
+const tareas = []
 
-setupCounter(document.querySelector('#counter'))
+//Al boton le añado un evento de click y una función anonima
+btnAdd.addEventListener('click', () => {
+
+  //Creo una variable donde guardo el texto que se introduce en el input
+  const textoAdd = document.querySelector('#tarea-introducida').value.trim()
+  if (textoAdd === "") {
+    //Muestro un mensaje de error en rojo gradiente
+
+    Toastify({
+      text: "Tarea introducida vacía",
+      duration: 3000,
+      style:
+      {
+        background: "linear-gradient(to right, #ff416c, #ff4b2b)",
+      }
+    }).showToast();
+
+   
+
+  }
+  else { 
+    //Añado la tarea a la lista
+    addTareasFunction(textoAdd);
+  }
+
+
+
+})
+
+
+
+function addTareasFunction(textoAdd) {
+  //Añado la tarea a la lista
+  tareas.push(textoAdd);
+  //Muestro la tarea en la consola
+  console.log("Tarea añadida: " + tareas);
+
+  //Creo una variable donde guardo el elemento que representa a la lista
+  const listaTareas = document.querySelector('#lista-tareas')
+
+  //Creo una variable donde guardo el elemento li (cada elemento concreto)
+  const tarea = document.createElement('li')
+  tarea.textContent = textoAdd
+
+  //Añado la tarea como li al div de ul
+  listaTareas.appendChild(tarea)
+  Toastify({
+    text: "Tarea introducida vacía",
+    duration: 3000,
+    style:
+    {
+      background: "linear-gradient(to right, #00b09b, #96c93d)",
+    }
+  }).showToast();
+
+
+
+}
+
